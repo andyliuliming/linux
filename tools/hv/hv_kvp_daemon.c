bbd8a781d3a8d3f449b8496640796c32ecd5db6c
Andy Liu <andyliuliming@outlook.com>
2018-08-09 16:25:53 +0800

    ifx.

--- tools/hv/hv_kvp_daemon.c
+++ tools/hv/hv_kvp_daemon.c
@@ -1512,7 +1512,7 @@ int main(int argc, char *argv[])
 			}
 			continue;
 		}
-
+		syslog(LOG_WARNING, "#KVP daemon gotiiii.")
 		syslog(LOG_WARNING, "#KVP daemon got operation: %d.", op);
 		switch (op) {
 		case KVP_OP_GET_IP_INFO:
